040f8d127e94949a006df2f1718b1b8e500afebe
roshanthamalaka <roshanthamalaka@gmail.com>
2022-12-02 21:56:46 +0530

    Add project files.

---
+++ Testing/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int number;
+string input;
+
+Console.WriteLine("Enter Number : ");
+input = Console. ReadLine();
+number = Convert.ToInt32(input);
+
+if (number == 2)
+{
+    Console.WriteLine("Dekai Hutto");
+
+}
+else
+{
+
+    Console.WriteLine("Hukahan Hutto"); 
+}
+
+Console.WriteLine("Ehena Kollo APi giya");
+
+
